e6464edbba2d4c90a7f2a1ebd1841829a760a381
Andy Catlin <andycatlin@hotmail.com>
2012-10-30 15:52:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boardgame::Application.config.secret_token = 'dff4744e0cb3bd30d21a1931cc25845e5aaafc6a80297a906dbdbc222f9935e9bb64f47c58c1f5e72e3bb6ce9370ef6eb15e4fdfd5a1b9b0b8137a0582c0588e'
